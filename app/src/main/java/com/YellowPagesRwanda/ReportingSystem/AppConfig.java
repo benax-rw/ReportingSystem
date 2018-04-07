4bc3df2dba0054ee49e586718f1a8023259f03b9
Benax <baziramwabo@gmail.com>
2018-04-07 23:14:09 +0900

    Update AppConfig.java

--- app/src/main/java/com/YellowPagesRwanda/ReportingSystem/AppConfig.java
+++ app/src/main/java/com/YellowPagesRwanda/ReportingSystem/AppConfig.java
@@ -4,7 +4,7 @@ public class AppConfig {
 
     public static String URL_LOGIN = "http://192.168.0.21/reportingsystem/login.php";
     public static String URL_REGISTER = "http://192.168.0.21/reportingsystem/register.php";
-    public static String URL_ADD_NOTE = "http://192.168.0.21/reportingsystemadd/Note.php";
-    public static String URL_GET_NOTE = "http://192.168.0.21/reportingsystemget/Note.php";
+    public static String URL_ADD_NOTE = "http://192.168.0.21/reportingsystemadd/addNote.php";
+    public static String URL_GET_NOTE = "http://192.168.0.21/reportingsystemget/getNote.php";
 
 }
